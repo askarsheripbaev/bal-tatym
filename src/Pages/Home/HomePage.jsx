8c66f797f03c285d38ed04bf9eb5ba353b5c797d
askarsheripbaev <sheripbaev1221@gmail.com>
2023-02-08 21:25:45 +0600

    update

--- src/Pages/Home/HomePage.jsx
+++ src/Pages/Home/HomePage.jsx
@@ -4,11 +4,7 @@ import s from './HomePage.module.css'
 const HomePage = () => {
     return (
         <div>
-<<<<<<< HEAD:src/Pages/HomePage.jsx
             Home
-=======
-
->>>>>>> 077d04424365bd9db55e5d17f0c67754a29fa746:src/Pages/Home/HomePage.jsx
         </div>
     );
 };
