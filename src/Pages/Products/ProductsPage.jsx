2282995804b1fee10caa08329a290c36979d42b0
askarsheripbaev <sheripbaev1221@gmail.com>
2023-02-10 17:02:59 +0600

    delete forms

--- src/Pages/Products/ProductsPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ProductsPage = () => {
-  return (
-    <div>Продукция</div>
-  )
-}
-
-export default ProductsPage
--- src/Pages/ShoppingCart/ShoppingCartPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ShoppingCartPage = () => {
-  return (
-    <div>Корзина</div>
-  )
-}
-
-export default ShoppingCartPage
